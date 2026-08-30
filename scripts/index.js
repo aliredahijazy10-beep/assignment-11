152aacd7ee52e88c66a97561ba21f18c340e3fe3
aliredahijazy10-beep <alireda.hijazy.10@gmail.com>
2026-08-30 22:16:51 +0300

    feat: add js file

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+const user_name = "Ali Reda Hijazi";
+let age = 16;
+
+console.log(`My name is ${user_name}. I'm ${age} years old`);
+
+age = 14;
+
+console.log(`2 years ago, I was ${age} years ago`);
